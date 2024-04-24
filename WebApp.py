785d5e2c98369787b9fe8fee37f274fbc3c5de18
LukeCorcoran99 <124389584+LukeCorcoran99@users.noreply.github.com>
2024-04-24 16:08:29 -0700

    'More Info' feature implemented

--- WebApp.py
+++ WebApp.py
@@ -25,6 +25,26 @@ def fetchPic(movie_id):
 
     return full_path
 
+# Function to fetch the movie poster using the movie ID from The Movie Database (TMDb)
+def fetchHomepage(movie_id):
+    # Creating the URL to request movie data from TMDb
+    url = "https://api.themoviedb.org/3/movie/{}?language=en-US".format(movie_id)
+    
+    # Setting up the required headers for the API request
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTcxYmM3MzkzNTc4MGE5OWY3NzhkOGZhYjIwMjA4NCIsInN1YiI6IjY2MjE5NWEyZTRjOWViMDE3Y2Y2MDMyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ju3GQ931E3dg3F-SqP9G8-v1q80dqHN4so3QZcSi91o"
+    }
+    
+    # Sending the request and getting the response
+    response = requests.get(url, headers=headers)
+    response = response.json()
+    
+    # Extracting the homepage path from the response
+    homepage = response['homepage']
+
+    return homepage
+
 # Function that recommends similar movies when a movie is passed through as an argument
 def recommendMovie(movie):
     # Find the index of the movie in the data frame based on its title
@@ -33,9 +53,10 @@ def recommendMovie(movie):
     # Sort the indices of movies based on their cosine similarity scores in descending order
     distances = sorted(list(enumerate(similarities[index])), reverse=True, key=lambda x: x[1])
     
-    # Lists to store recommended movie names and their posters
+    # Lists to store recommended movie names and their posters, and links to info
     recMovieNames = []
     recMoviePics = []
+    recMovieHomepage = []
     
     # Printing titles of the recommended movies
     for i in distances[1:6]:
@@ -43,8 +64,9 @@ def recommendMovie(movie):
         movie_id = movies.iloc[i[0]].movie_id
         recMoviePics.append(fetchPic(movie_id))
         recMovieNames.append(movies.iloc[i[0]].title)
+        recMovieHomepage.append(fetchHomepage(movie_id))
 
-    return recMovieNames, recMoviePics
+    return recMovieNames, recMoviePics, recMovieHomepage
 
 # Display header
 sl.header("MovieMate")
@@ -62,23 +84,29 @@ chosenMovie = sl.selectbox('Enter/choose a movie to receive a recommendation', m
 # Button to trigger the recommendation process
 if sl.button('Go'):
     # Getting recommended movie names and their poster URLs
-    recMovieNames, recMoviePics = recommendMovie(chosenMovie)
+    recMovieNames, recMoviePics, recMovieHomepage = recommendMovie(chosenMovie)
     
     # Displaying recommended movies and their posters in columns
     col1, col2, col3, col4, col5 = sl.columns(5)
     with col1:
         sl.text(recMovieNames[0])
         sl.image(recMoviePics[0])
+        sl.markdown("[More info](%s)" % recMovieHomepage[0])
     with col2:
         sl.text(recMovieNames[1])
         sl.image(recMoviePics[1])
+        sl.markdown("[More info](%s)" % recMovieHomepage[1])
     with col3:
         sl.text(recMovieNames[2])
         sl.image(recMoviePics[2])
+        sl.markdown("[More info](%s)" % recMovieHomepage[2])
     with col4:
         sl.text(recMovieNames[3])
         sl.image(recMoviePics[3])
+        sl.markdown("[More info](%s)" % recMovieHomepage[3])
     with col5:
         sl.text(recMovieNames[4])
         sl.image(recMoviePics[4])
+        sl.markdown("[More info](%s)" % recMovieHomepage[4])
+
 
